066f42d54173257cc8f5fd77aaf1c4975465010e
carolineflora95 <139001115+carolineflora95@users.noreply.github.com>
2023-08-25 15:37:23 +0200

    fixed bugs and added back button

--- src/index.js
+++ src/index.js
@@ -3,52 +3,71 @@ function displayTime() {
   let skiptonDate = moment()
     .tz("Europe/London")
     .format("dddd, Do MMMM YYYY 📅");
-  skiptonDateElement.innerHTML = skiptonDate;
+  if (skiptonDateElement !== null) {
+    skiptonDateElement.innerHTML = skiptonDate;
+  }
 
   let skiptonTimeElement = document.querySelector("#skipton .time");
   let skiptonTime = moment().tz("Europe/London").format("HH:mm:ss A 🕖");
-  skiptonTimeElement.innerHTML = skiptonTime;
+
+  if (skiptonTimeElement !== null) {
+    skiptonTimeElement.innerHTML = skiptonTime;
+  }
 
   let cuscoDateElement = document.querySelector("#cusco .date");
   let cuscoDate = moment().tz("America/Lima").format("dddd, Do MMMM YYYY 📅");
-  cuscoDateElement.innerHTML = cuscoDate;
-
+  if (cuscoDateElement !== null) {
+    cuscoDateElement.innerHTML = cuscoDate;
+  }
   let cuscoTimeElement = document.querySelector("#cusco .time");
   let cuscoTime = moment().tz("America/Lima").format("HH:mm:ss A 🕖");
-  cuscoTimeElement.innerHTML = cuscoTime;
-
+  if (cuscoTimeElement !== null) {
+    cuscoTimeElement.innerHTML = cuscoTime;
+  }
   let melbourneDateElement = document.querySelector("#melbourne .date");
   let melbourneDate = moment()
     .tz("Australia/Melbourne")
     .format("dddd, Do MMMM YYYY 📅");
-  melbourneDateElement.innerHTML = melbourneDate;
-
+  if (melbourneDateElement !== null) {
+    melbourneDateElement.innerHTML = melbourneDate;
+  }
   let melbourneTimeElement = document.querySelector("#melbourne .time");
   let melbourneTime = moment()
     .tz("Australia/Melbourne")
     .format("HH:mm:ss A 🕖");
-  melbourneTimeElement.innerHTML = melbourneTime;
+  if (melbourneTimeElement !== null) {
+    melbourneTimeElement.innerHTML = melbourneTime;
+  }
 }
 
 function updateLocation(event) {
   let locationTimeZone = event.target.value;
+  let interfaceElement = document.querySelector("#cities");
   if (locationTimeZone === "current") {
     locationTimeZone = moment.tz.guess();
-    document.querySelector("#refreshLink").style.display = "none";
+  } else if (locationTimeZone === "nocity") {
+    interfaceElement.innerHTML = `<div>Select a fucking city</div>`;
+    return;
   }
   let locationName = locationTimeZone.replace("_", " ").split("/")[1];
   let locationTime = moment().tz(locationTimeZone);
-  let interfaceElement = document.querySelector("#cities");
   interfaceElement.innerHTML = `
-   <div>
+   <div class="city" id="${locationName}">
           <h2>${locationName}</h2>
           <div class="date">${locationTime.format("MMMM Do YYYY")}</div>
           <div class="time">${locationTime.format(
             "h:mm"
           )} <small>${locationTime.format("A")}</small></div>
+         <br />
         </div>
+        <div class="anchor-right">
+            <a href="index.html" class="anchor">🔙</a>
+        </div>
+        
+
   `;
 }
+displayTime();
 setInterval(displayTime, 1000);
 
 let locationSelectElement = document.querySelector("#city");
